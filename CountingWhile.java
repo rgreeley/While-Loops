74bff8777d514e0dee662bd48c6baca8bf4f5d72
Robert Greeley <rgreeley@zappos.com>
2014-08-06 11:28:38 -0700

    Counting While EX51

---
+++ CountingWhile.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class CountingWhile
+{
+		public static void main( String[] args )
+		{
+				Scanner keyboard = new Scanner(System.in);
+				
+				System.out.println( "Type in a message, and I'll display it several times." );
+				System.out.print( "Message: " );
+				String message = keyboard.nextLine();
+				
+				System.out.print( "How many times would you like?" );
+				int entry = keyboard.nextInt();
+				
+				int n = 0;
+				while ( n < entry )
+				{
+						System.out.println((n+(n*10)) + ". " + message );
+						n++;
+				}
+			}
+}
